f8836dabc2f97a33719be1d2d3ea42de1a336ffa
tedrickwong <tedrickwong8@gmail.com>
2019-08-03 00:35:55 -0700

    changing layout

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() 
 {
-    var rows = document.getElementsByClassName("lCol");
+    /*var rows = document.getElementsByClassName("lCol");
     var images = document.getElementsByClassName("rCol");
     
     for(var i = 0;i < rows.length;i++)
     {
         images[i].style.height = rows[i].clientHeight+"px";
-    }
+    }*/
 
 
 });
