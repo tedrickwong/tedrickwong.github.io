f782949bd725043fecb24fc084a31e165dfd779b
tedrickwong <tedrickwong8@gmail.com>
2019-09-02 21:20:53 -0700

    Updates to nav bar and page titles

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ $( document ).ready(function()
     {
         images[i].style.height = rows[i].clientHeight+"px";
     }*/
+	
+	var pages = document.getElementById('navigation').getElementsByTagName('a');
+
+	for( i=0; i< pages.length; i++ )
+	{
+		var page = pages[i];
+		var found = false;
+		
+		if (location.href.includes(page.innerText.toLowerCase().split(" ")[0]))
+		{
+			page.firstElementChild.style.color = "black";
+			page.firstElementChild.style.backgroundColor = "white";	
+			found = true;
+			break;
+		}
+		
+		if (found == false && i == pages.length - 1)
+		{
+			document.getElementById("home").style.color = "black";
+			document.getElementById("home").style.backgroundColor = "white";	
+		}
+	}
 
 
 });
