b588763229bd0347f563c4b85874cf0a8c33f7ef
tedrickwong <tedrickwong8@gmail.com>
2019-09-19 19:48:09 -0700

    Consolidating tabs into a single page application, adding fade in animation, and updating navigation to be in the sidebar

--- index.js
+++ index.js
@@ -1,52 +1,14 @@
 $( document ).ready(function() 
 {
-    /*var rows = document.getElementsByClassName("lCol");
-    var images = document.getElementsByClassName("rCol");
-    
-    for(var i = 0;i < rows.length;i++)
-    {
-        images[i].style.height = rows[i].clientHeight+"px";
-    }*/
-	
-	/* var pages = document.getElementById('navigation').getElementsByTagName('a');
+   $(".icon, .text").on("click",function(event){
 
-	for( i=0; i< pages.length; i++ )
-	{
-		var page = pages[i];
+		var section = "#"+this.parentNode.id + "-section";
+		console.log(section);
+		$('html, body').animate({
+			scrollTop: $(section).offset().top},
+			800);
 		
-		if (location.href.includes(page.innerText.toLowerCase().split(" ")[0]))
-		{
-			page.firstElementChild.style.color = "black";
-			page.firstElementChild.style.backgroundColor = "white";	
-			break;
-		}
-		
-		if (i == pages.length - 1)
-		{
-			document.getElementById("home").style.color = "black";
-			document.getElementById("home").style.backgroundColor = "white";	
-		}
-	} */
+   });
 
 
 });
-
-/* function openPage(pagename){
-	switch(pagename)
-	{
-		case "home":
-			location.href="index.html";
-			break;
-		case "experience":
-			location.href="experience";
-			break;
-		case "projects":
-			location.href="projects";
-			break;
-		case "contact me":
-			location.href="contact";
-			break;
-	}
-	document.getElementById(pagename).style.color = "black";
-	document.getElementById(pagename).style.backgroundColor = "white";
-} */
