1f7769d643ae260fd40219c137ff08a293fc4673
tedrickwong <tedrickwong8@gmail.com>
2020-08-02 21:47:44 -0700

    [v2.2] updating color scheme, bootstrap version, font. adding badges for skills subsection and cards for projects

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $( document ).ready(function()
    $(".nav-btn").on("click",function(event){
 
 		var section = "#"+this.id + "-section";
-		console.log(section);
+		
 		$('html, body').animate({
 			scrollTop: $(section).offset().top - 80},
 			800);
@@ -13,16 +13,27 @@ $( document ).ready(function()
    window.onscroll = function() {stickyNav()};
 
    var navbar = document.getElementById("navbar");
+   var navbtn = document.getElementsByClassName("nav-btn");
    var offset = navbar.offsetTop;
    
    function stickyNav(){
 		if( window.pageYOffset > offset )
 		{
 			navbar.classList.add("sticky");
+			navbar.style.color = "#FFFFFF";
+			for(var i = 0;i < navbtn.length;i++)
+			{
+				navbtn[i].style.borderBottom = "3px solid #FFFFFF";
+			}
 		}
 		else
 		{
 			navbar.classList.remove("sticky");
+			navbar.style.color = "#22313f";
+			for(var i = 0;i < navbtn.length;i++)
+			{
+				navbtn[i].style.borderBottom = "3px solid #22313f";
+			}			
 		}
    }
 
